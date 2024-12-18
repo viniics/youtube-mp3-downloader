dac30cc58cda92c33707c72d9a38bf09c18ad76c
Vinicius <viniipvp@gmail.com>
2024-12-18 09:51:13 -0300

    ok

--- app/app.py
+++ app/app.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, request, jsonify
-from utils import baixar_musica  # Importando a função de download
+from utils import baixar_musica
 import webbrowser
-# Criando o aplicativo Flask
 app = Flask(__name__)
-
-# exibir o formulário
+initalized = False
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -15,19 +13,21 @@ def baixar_audio():
     # Receber a URL do formulário
     url = request.form.get('url')
     
-    if not url:
-        return jsonify({'error': 'URL não fornecida'}), 400
-    
     try:
         # Chamar a função de download
         caminho_arquivo = baixar_musica(url)
-        return jsonify({'success': True, 'caminho': caminho_arquivo})
+        return jsonify({'success': True, 'download': caminho_arquivo})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 def abrir_navegador():
-    webbrowser.open_new("http://127.0.0.1:5000")
+    #webbrowser.open_new("http://127.0.0.1:5000")
+    print("ok")
+
+def initialize():
+    abrir_navegador()
+
 
 if __name__ == '__main__':
-    abrir_navegador()
+    initialize()
     app.run(debug=True)
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,22 +1,33 @@
 const form = document.querySelector("form")
-const response = document.getElementById("response")
+const responseElement = document.getElementById("response")
+
+function clearAtSubmit(){
+        responseElement.innerHTML=``
+}
+
+function toggleButtonVisibility(isLoading){
+    const button = document.getElementById("submit-btn")
+    if (isLoading){
+        button.style.display = "none";
+    }
+    else{button.style.display = "inline-block"}
+}
 
 form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    clearAtSubmit();
+    toggleButtonVisibility(true);
     const formData = new FormData(form);
-
     try {
         const response = await fetch('/baixar_audio', {
             method: 'POST',
             body: formData
         });
-
-        const result = await response.json();
-        if (response.ok) {
-            response.innerHTML = `<p>tudo ok</p>`
-        }
     }
     catch (error) {
-        response.innerHTML = `<p>Deu ruim</p>`
+    }
+    finally{
+        toggleButtonVisibility(false)
     }
 }
 )
